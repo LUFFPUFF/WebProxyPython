b2d4d01f3c3ad8eb93e9e85f21773acc4e0a0da7
LUFFPUFF <beatsluffi@gmail.com>
2024-06-03 17:57:00 +0300

    compleat

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, render_template
 import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Прокси-сервер работает!'
+    return render_template('index.html')
 
 @app.route('/proxy', methods=['GET', 'POST'])
 def proxy():
-    url = request.args.get('url')  # Получаем URL из параметра запроса
-
-    if not url:
-        return jsonify({'error': 'Не указан URL'}), 400
-
-    try:
-        # Отправляем запрос к указанному URL и возвращаем результат
-        response = requests.get(url, params=request.args, headers=request.headers)
-        return response.content, response.status_code, response.headers.items()
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
+    if request.method == 'POST':
+        url = request.form.get('url')
+        if url:
+            try:
+                response = requests.get(url)
+                print(f'Request URL: {url}, Response Status: {response.status_code}')
+                return response.content
+            except requests.RequestException as e:
+                print(f'Error: {e}')
+                return str(e), 500
+    return '', 400
 
 if __name__ == '__main__':
     app.run(debug=True)
